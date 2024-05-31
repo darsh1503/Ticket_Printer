6f5c39514f3dc91e8b2b52fdcdbee22f2226b6d6
DARSH PATEL <81702502+darsh1503@users.noreply.github.com>
2024-05-30 22:16:02 -0400

    Adding validation...

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-let pageCount = 0;
+//let pageCount = 0;
 
-function addNumber(number) {
+
+
+/*function addNumber(number) {
     pageCount = number;
     document.getElementById('pageCount').innerText = pageCount;
+}*/
+
+
+
+let pageCount = '';
+
+function addNumber(number) {
+    // Check if the input is a number
+    if (!isNaN(number)) {
+        // Check if the count exceeds a certain limit (if needed)
+        if (pageCount.length < 2) {
+            if (pageCount === '0') {
+                pageCount = number.toString();
+            } else {
+                pageCount += number.toString();
+            }
+        } else {
+            alert('Maximum ticket count reached (99).');
+        }
+    } else {
+        alert('Please enter a valid number.');
+    }
+
+    // Update the display
+    document.getElementById('pageCount').innerText = pageCount;
 }
+
+
 
 function clearScreen() {
     pageCount = 0;
